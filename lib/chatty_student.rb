e0609fa7fd86a5faf8ad256e1fe35c3e12e8eabe
Luis Castillo <raul@Luiss-MacBook-Pro.local>
2021-01-16 23:49:01 -0400

    commit tree f35fde76716164f3e3e26e34f02e04213f03dc34

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -11,3 +11,18 @@ def raise_hand
 end
 
 end
+
+# ChattyStudent
+#   #hello
+#     returns a greeting phrase plus a long chatty phrase
+#   #raise_hand
+#     returns the phrase 'Pick me!' ten times.
+
+# Student
+#   #hello
+#     returns a greeting phrase
+#   #raise_hand
+#     returns the phrase 'Pick me!'
+
+# Finished in 0.00362 seconds (files took 0.1165 seconds to load)
+# 4 examples, 0 failures
