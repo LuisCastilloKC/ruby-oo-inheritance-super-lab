8ff6d5bb2a85cd2c1c36bfac49265e61f13c119f
Luis Castillo <raul@Luiss-MacBook-Pro.local>
2021-01-16 23:49:05 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,8 +1,26 @@
 class Student
     
     def hello
-
+        puts "Hey there! I'm so excited to learn stuff.\n"
     end
 
+    def raise_hand
+        puts "Pick me!\n"
+    end
 
 end
+
+# ChattyStudent
+#   #hello
+#     returns a greeting phrase plus a long chatty phrase
+#   #raise_hand
+#     returns the phrase 'Pick me!' ten times.
+
+# Student
+#   #hello
+#     returns a greeting phrase
+#   #raise_hand
+#     returns the phrase 'Pick me!'
+
+# Finished in 0.00362 seconds (files took 0.1165 seconds to load)
+# 4 examples, 0 failures
